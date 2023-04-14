9fedddefaa1b60f188aac13a400700cf0e8b671a
atbreeze <ssuatbreeze@kakao.com>
2023-04-14 14:30:43 +0900

    initial 10

---
+++ src/main/java/PreProject/demo/RecommendationsController.java
@@ -0,0 +1,15 @@
+package PreProject.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecommendationsController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "!!";
+    }
+
+}
